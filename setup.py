a62309254e23f54100a3662c48a1691af76e3fda
Sergey Gasilov <gasilos@wks-l001944.clsi.ca>
2021-01-04 12:56:37 -0600

    commit 1b

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(name='ezufo_helpers',
     description='BMIT helper tools for uCT data processing',
     author=['Sergei Gasilov', 'Toby Bond'],
     url='https://bmit.lightsource.ca',
-    packages=['ez_stitch, ez_mview, ez_360_find_overlap'],
+    packages=['ez_stitch', 'ez_mview', 'ez_360_find_overlap'],
     scripts=['bin/ezstitch', 'bin/ezmview', 'bin/ez360_find_overlap', 'bin/ez360_multi_stitch']
     )
 
